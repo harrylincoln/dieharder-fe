fbb03bf523dae4558618992b26df1b9c2366e9c7
Harry Lincoln <v.harry.lincoln@red.virgin.com>
2021-12-28 23:01:05 +0000

    logos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { ReactComponent as Logo } from "../logo2.svg";
+
 const Home = () => (
   <>
-    <div>LOGO</div>
+    <Logo />
+    <h1>
+      NAKATOMI <br />
+      PLAZA
+    </h1>
+
     <Link to="/directory">TOUCH SCREEN TO START DIRECTORY</Link>
   </>
 );
