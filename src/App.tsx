567a40e4fc2ec8ae2a40ea1e62f6b7b99510d13b
harrylincoln <harrylincoln0612@gmail.com>
2022-11-29 17:50:36 +0000

    add GA

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReactGA from "react-ga";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -7,15 +8,23 @@ import Quiz from "./pages/Quiz";
 import PlazaDirectory from "./pages/PlazaDirectory";
 import PlazaDirectoryLetter from "./pages/PlazaDirectoryLetter";
 
-const App = () => (
-  <div className="App">
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/quiz" element={<Quiz />} />
-      <Route path="/directory" element={<PlazaDirectory />} />
-      <Route path="/directory/:letter" element={<PlazaDirectoryLetter />} />
-    </Routes>
-  </div>
-);
+ReactGA.initialize("G-WTQEXY94XL");
+
+const App = () => {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/directory" element={<PlazaDirectory />} />
+        <Route path="/directory/:letter" element={<PlazaDirectoryLetter />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
